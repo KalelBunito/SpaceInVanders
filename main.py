9cab30bf18262d91bdf1bced887938f65f6c3faf
KalelBunito <kalelhbarboza@gmail.com>
2024-11-22 17:02:10 -0300

    primeirocommit

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ pygame.init()
 
 # Configuraçoes iniciasis de tela
 screen = pygame.display.set_mode((800,600))
-screen.fill((0, 0, 0))
 pygame.display.set_caption("Player Game")
 
 player = Player(370, 500, 50)
 
 while True:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    # VERIFICAR SE AS TECLAS FORAM PRESSIONADAS
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+            player.move_left()
 
-player.draw(screen)
-pygame.display.update.update()
+        elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+            player.move_right() 
+
+    player.draw(screen)
+    pygame.display.update()
--- player.py
+++ player.py
@@ -6,6 +6,15 @@ class Player:
         self.y = y
         self.image = pygame.image.load('PlayerSpace.png')
         self.image = pygame. transform.scale(self.image, (size, size))
+        self.size = 20
+        self.speed = 2
+
+    def move_left(self):
+        if self.x > 0:
+            self.x -= self.speed
+    def move_right(self):
+        if self.x < 800 - self.size:
+            self.x += self.speed
 
     def draw(self, screen):
-        screen.blit(self.image(self.x, self.y))
+        screen.blit(self.image,  (self.x, self.y))
